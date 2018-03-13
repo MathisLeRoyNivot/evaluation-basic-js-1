e8c2f88b6361510ae19a004438317fba8700e301
Mathis Le Roy-Nivot <mathis.leroynivot@ynov.com>
2018-03-13 10:47:18 +0100

    exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,6 +1,29 @@
 var applyMap = function() {
   // Your future job begins here ...
-}
+  var inputValues = [12, 26, 42]
+  function map1(value) {
+    return value + 10
+  }
+  function map2(value) {
+    return value * 3
+  }
+  var inputMaps = [map1, map2]
+
+  var applyMap = function(inputValues, inputMaps) {
+  if (inputValues === undefined || inputMaps === undefined) {
+  var output = [];
+  return output;
+  }
+  else {
+    var output = [];
+    var mapping = [];
+    for (var i = 0; i < inputMaps.length; i++) {
+      mapping = inputValues.map(inputMaps[i]);
+      output.push(mapping);
+    }
+    return output;
+  }  
+ }
 
 module.exports = {
   title: 'Exercise 3',
