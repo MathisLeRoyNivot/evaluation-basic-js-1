9ac223db36d539dbebe3fe992d2b8260f83c6ad2
Mathis Le Roy-Nivot <mathis.leroynivot@ynov.com>
2018-03-13 10:47:02 +0100

    exercise 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,5 +1,30 @@
 var mapCustomer = function(customer) {
   // Your future job begins here ...
+  var input = {
+  firstname: 'Jack',
+  lastname: 'Smith',
+  products: [{ label: 'shoes', price: 89 }, { label: 'book', price: 14 }, { label: 'smartphone', price: 499 }]
+  }
+
+  var mapCustomer = function(input) {
+    if (input != {}) {
+      var output = Object.assign({}, input);
+      var sum = 0;
+      for (var i = 0; i < output.products.length; i++) {
+        sum += output.products[i].price;
+      }
+    output.moneySpent = Number(sum);
+    return output;
+    }  else {
+      var output = {
+        firstname: 'John',
+        lastname: 'Doe',
+        products: [],
+        moneySpent: 0
+      };
+      return output;
+    }
+  }
 }
 
 module.exports = {
