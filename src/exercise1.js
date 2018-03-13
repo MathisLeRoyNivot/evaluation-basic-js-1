1479dea928e4f8905a56d8f6b67a916ce29211a3
Mathis Le Roy-Nivot <mathis.leroynivot@ynov.com>
2018-03-13 10:22:04 +0100

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,5 +1,15 @@
 var removeNegativeValue = function(values) {
   // Your future job begins here ...
+  var arrNumbers=[34, -5, 9, 18, 0, -7, 32];
+  var result = arrNumbers.filter(
+    function (value) {
+      return (typeof value === 'string');
+    });
+    return result
+  // return arrNumbers.filter(typeof value === 'string');
+  var arrNumbers1=[34, -5, 9, 18, 0, -7, 32];
+  const result1 = arrNumbers1.filter(number => number < 0);
+  return result1;
 }
 
 module.exports = {
